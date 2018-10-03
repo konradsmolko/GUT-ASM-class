87e94c812686cc71e3f667c5808dd6a73001231a
konrad <17689319+konradsmolko@users.noreply.github.com>
2018-10-03 19:49:03 +0200

    Add project files.

---
+++ cwiczenie1/main.asm
@@ -0,0 +1,12 @@
+.686
+.model flat
+
+public  main
+
+.data
+
+.code
+main PROC
+	mov rax, 255
+main ENDP
+END
---
+++ polskie_znaki/test.asm
@@ -0,0 +1,13 @@
+.686
+.model flat
+
+public _main
+
+.data
+
+
+.code
+_main PROC
+	
+_main ENDP
+END

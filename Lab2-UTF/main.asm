c451e196b42b1824f4dc4760eb48e3be0a07795d
konrad <17689319+konradsmolko@users.noreply.github.com>
2018-10-25 00:36:18 +0200

    switch back to x32 asm because calling c stuff in x64 is a pain

--- Lab2-UTF/main.asm
+++ Lab2-UTF/main.asm
@@ -1,9 +1,13 @@
+; wczytywanie i wyświetlanie tekstu wielkimi literami
+; (inne znaki się nie zmieniają)
+.686
+.model flat
 extern _ExitProcess@4 : PROC
-extern __write : PROC ; (dwa znaki podkreslenia)
-extern __read  : PROC ; (dwa znaki podkreslenia)
+extern __write : PROC ; (dwa znaki podkreślenia)
+extern __read  : PROC ; (dwa znaki podkreślenia)
 public  _main
 .data
-tekst_pocz	db 10, 'Prosze napisa? jakis tekst '
+tekst_pocz	db 10, 'Proszę napisać jakiś tekst '
 			db 'i nacisnac Enter', 10
 koniec_t	db ?
 magazyn		db 80 dup (?)
@@ -11,49 +15,47 @@ nowa_linia	db 10
 liczba_znakow dd ?
 .code
 _main:
-	; wyswietlenie tekstu informacyjnego
-	; liczba znakow tekstu
-	mov     ecx, (OFFSET koniec_t) - (OFFSET tekst_pocz)
+	; wyświetlenie tekstu informacyjnego
+	; liczba znaków tekstu
+	mov     ecx, (OFFSET koniec_t)-(OFFSET tekst_pocz)
 	push    ecx
 	push    OFFSET tekst_pocz  ; adres tekstu
-	push	1 ; nr urz?dzenia (tu: ekran - nr 1)
-	call    __write  ; wyswietlenie tekstu pocz?tkowego
-	add     esp, 12 ; usuniecie parametrow ze stosu
+	push	1 ; nr urządzenia (tu: ekran - nr 1)
+	call    __write  ; wyświetlenie tekstu początkowego
+	add     esp, 12	; usuniecie parametrów ze stosu
 	; czytanie wiersza z klawiatury
-	push    80 ; maksymalna liczba znakow
+	push    80 ; maksymalna liczba znaków
 	push    OFFSET magazyn
-	push    0  ; nr urz?dzenia (tu: klawiatura - nr 0)
-	call    __read ; czytanie znakow z klawiatury
-	add     esp, 12 ; usuniecie parametrow ze stosu
-	; kody ASCII napisanego tekstu zosta?y wprowadzone
+	push    0  ; nr urządzenia (tu: klawiatura - nr 0)
+	call    __read ; czytanie znaków z klawiatury
+	add     esp, 12	; usuniecie parametrów ze stosu
+	; kody ASCII napisanego tekstu zostały wprowadzone
 	; do obszaru 'magazyn'
-	; funkcja read wpisuje do rejestru EAX liczbe
-	; wprowadzonych znakow
+	; funkcja read wpisuje do rejestru EAX liczbę
+	; wprowadzonych znaków
 	mov     liczba_znakow, eax
-	; rejestr ECX pe?ni role licznika obiegow petli
+	; rejestr ECX pełni rolę licznika obiegów pętli
 	mov     ecx, eax
-	mov     ebx, 0
-	; indeks pocz?tkowy
-	ptl:  
+	mov     ebx, 0	; indeks początkowy
+ptl:  
 	mov     dl, magazyn[ebx] ; pobranie kolejnego znaku
 	cmp     dl, 'a'
 	jb      dalej   ; skok, gdy znak nie wymaga zamiany
 	cmp     dl, 'z'
 	ja      dalej   ; skok, gdy znak nie wymaga zamiany
 	sub     dl, 20H ; zamiana na wielkie litery
-	; odes?anie znaku do pamieci
+	; odesłanie znaku do pamięci
 	mov     magazyn[ebx], dl
-	dalej:
-	inc     ebx 
-	; inkrementacja indeksu
-	loop    ptl     ; sterowanie petl?
-	; wyswietlenie przekszta?conego tekstu
+dalej:
+	inc     ebx ; inkrementacja indeksu
+	loop    ptl     ; sterowanie pętlą
+	; wyświetlenie przekształconego tekstu
 	push  	liczba_znakow
 	push    OFFSET magazyn
 	push    1
-	call    __write  ; wyswietlenie przekszta?conego tekstu
-	add     esp, 12  ; usuniecie parametrow ze stosu
+	call    __write  ; wyświetlenie przekształconego 	tekstu
+	add     esp, 12  ; usuniecie parametrów ze stosu
 	push    0
 	call    _ExitProcess@4   
-	; zako?czenie programu
+	; zakończenie programu
 END
--- cwiczenie1/main.asm
+++ cwiczenie1/main.asm
@@ -4,7 +4,7 @@ extern ExitProcess  : PROC
 
 public main
 .data
-tekst	db 0Ah, 'Nazywam si', 0Eh, 0Ah, 'Konrad Smolko' , 0Ah
+tekst	db 0Ah, 'Nazywam się Konrad Smolko', 0Ah
 		db 'Moj pierwszy 64-bitowy program asemblerowy '
 		db 'dziala juz poprawnie!', 0Ah
 .code
@@ -13,7 +13,7 @@ main PROC
 	mov rdx, OFFSET tekst ; polozenie obszaru ze znakami
 	mov r8, 93	; liczba znakow wyswietlanego tekstu
 	sub rsp, 40	; przygotowanie obszaru na stosie dla funkcji _write
-	call _write	; wywolanie funkcji ”_write” z biblioteki jêzyka C
+	call _write	; wywolanie funkcji ”_write” z biblioteki języka C
 	add rsp, 40	; usuniêcie ze stosu wczesniej zarezerwowanego obszaru
 
 	sub rsp, 8	; wyrownanie zawartosci RSP, tak by byla podzielna przez 16

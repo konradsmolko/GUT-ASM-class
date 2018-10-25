df0549b5501b6d57a9eecf6f115133f1487658a7
konrad <17689319+konradsmolko@users.noreply.github.com>
2018-10-25 12:54:55 +0200

    finish lab 2, missing ex. 3

--- Lab2-UTF/main.asm
+++ Lab2-UTF/main.asm
@@ -1,59 +1,25 @@
+.686
+.model flat
+
 extern _ExitProcess@4 : PROC
-extern __write : PROC ; (dwa znaki podkreslenia)
-extern __read  : PROC ; (dwa znaki podkreslenia)
-public  _main
+extern _MessageBoxW@16 : PROC
+public _main
+; Zadanie 2.3
 .data
-tekst_pocz	db 10, 'Prosze napisa? jakis tekst '
-			db 'i nacisnac Enter', 10
-koniec_t	db ?
-magazyn		db 80 dup (?)
-nowa_linia	db 10
-liczba_znakow dd ?
+	tytul_Unicode	dw 'Z', 'n', 'a', 'k', 'i', 0
+	tekst_Unicode	dw 'O', 'x', ' ', 0D83Dh, 0DC02h, ' '
+					dw 'i', ' ', 'm', 'y', 's', 'z', ' ', 0D83Dh, 0DC01h, 0
+					;dw 1101 1000 0011 1101, 1101 1100 0000 0001
+					;1101 10 x F401
+					;1101 11 y
 .code
 _main:
-	; wyswietlenie tekstu informacyjnego
-	; liczba znakow tekstu
-	mov     ecx, (OFFSET koniec_t) - (OFFSET tekst_pocz)
-	push    ecx
-	push    OFFSET tekst_pocz  ; adres tekstu
-	push	1 ; nr urz?dzenia (tu: ekran - nr 1)
-	call    __write  ; wyswietlenie tekstu pocz?tkowego
-	add     esp, 12 ; usuniecie parametrow ze stosu
-	; czytanie wiersza z klawiatury
-	push    80 ; maksymalna liczba znakow
-	push    OFFSET magazyn
-	push    0  ; nr urz?dzenia (tu: klawiatura - nr 0)
-	call    __read ; czytanie znakow z klawiatury
-	add     esp, 12 ; usuniecie parametrow ze stosu
-	; kody ASCII napisanego tekstu zosta?y wprowadzone
-	; do obszaru 'magazyn'
-	; funkcja read wpisuje do rejestru EAX liczbe
-	; wprowadzonych znakow
-	mov     liczba_znakow, eax
-	; rejestr ECX pe?ni role licznika obiegow petli
-	mov     ecx, eax
-	mov     ebx, 0
-	; indeks pocz?tkowy
-	ptl:  
-	mov     dl, magazyn[ebx] ; pobranie kolejnego znaku
-	cmp     dl, 'a'
-	jb      dalej   ; skok, gdy znak nie wymaga zamiany
-	cmp     dl, 'z'
-	ja      dalej   ; skok, gdy znak nie wymaga zamiany
-	sub     dl, 20H ; zamiana na wielkie litery
-	; odes?anie znaku do pamieci
-	mov     magazyn[ebx], dl
-	dalej:
-	inc     ebx 
-	; inkrementacja indeksu
-	loop    ptl     ; sterowanie petl?
-	; wyswietlenie przekszta?conego tekstu
-	push  	liczba_znakow
-	push    OFFSET magazyn
-	push    1
-	call    __write  ; wyswietlenie przekszta?conego tekstu
-	add     esp, 12  ; usuniecie parametrow ze stosu
-	push    0
-	call    _ExitProcess@4   
-	; zako?czenie programu
+	push	0
+	push	OFFSET tytul_Unicode
+	push	OFFSET tekst_Unicode
+	push	0
+	call	_MessageBoxW@16
+
+	push	0
+	call	_ExitProcess@4
 END
---
+++ Lab2-UTF/main3.asm
@@ -0,0 +1,6 @@
+; int find(char* str, char c, int n)
+; str - wsk. do stringa
+; c - szukany znak
+; n - miejsce od którego szukam
+; ret - miejsce na ktorym jest znak (od poczatku stringa, nie od n). brak c = ret -1
+END

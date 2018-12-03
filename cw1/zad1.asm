52038c5f1aedaf44dfaae09c4ab14e0db6bc876f
konrad <17689319+konradsmolko@users.noreply.github.com>
2018-12-03 13:14:17 +0100

    finish homework

--- cw1/zad1.asm
+++ cw1/zad1.asm
@@ -4,40 +4,70 @@ public _main
 .data
 	bufor dq 256 dup (12345678ABCDEF0Eh)
 	wynik dq 256 dup (?)
+	granica	dd 11111111h ; debug - sprawdzenie czy petla nie wychodzi poza zakres wyniku
 .code
 _main PROC
 	pusha
 
 	mov		esi, OFFSET bufor
 	mov		edi, OFFSET wynik
-	mov		ecx, 256
-	xor		edx, edx
+	mov		ecx, 256*8
+	xor		ebx, ebx
+	xor		eax, eax
 
-ptl1:
-	mov		ebx, DWORD PTR [esi+edx*4]
-	bswap	ebx
-	mov		eax, DWORD PTR [esi+edx*4+4]
-	bswap	eax
-	mov		[esi+edx*4], eax
-	mov		[esi+edx*4+4], ebx
-	add		edx, 2
-	cmp		edx, 256
-	jl		ptl1
+	mov		dl, BYTE PTR [esi+ebx] ; 0Eh ; pierwsze 2 bajty
+	mov		dh, dl ; 0Eh ; skopiowanie
 
-	xor		edx, edx
-ptl2:
-	mov		ebx, DWORD PTR [esi+edx*4]
-	mov		eax, DWORD PTR [esi+edx*4+1]
+	mov		ah, dh
+	and		ah, 80h ; zapisanie pierwszego bitu
 
+	shl		dh, 1 ; przygotowanie do op. xnor
+	mov		al, BYTE PTR [esi+ebx+1]
+	rol		al, 1
+	and		al, 00000001b
+	and		dh, 11111110b
+	or		dh, al ; wpisanie ostatniego bitu z kolejnego bajtu
 
-	xor		ebx, eax
-	not		ebx
+	xor		dl, dh
+	not		dl ; główna funkcja dekodująca
 
-	mov		[edi+edx*4], ebx
-	inc		edx
-	cmp		edx, 256
+	ror		dx, 1 ; pierwszy bit dh = xnor 7 i 8 bitu danych
+	and		dl, 01111111b ; wyzerowanie 1 bitu wyniku
+	or		dl, ah ; wpisanie pierwszego bitu
+
+	mov		[edi+ebx], BYTE PTR dl
+
+	inc		ebx
+	dec		ecx
+main_loop:
+	mov		dl, BYTE PTR [esi+ebx] ; pierwsze 2 bajty
+	mov		ah, dh
+	and		ah, 80h ; zapisanie pierwszego bitu
+	mov		dh, dl ; skopiowanie
+
+	mov		al, BYTE PTR [esi+ebx+1]
+	rol		al, 1
+	and		al, 00000001b
+	shl		dh, 1 ; przygotowanie do op. xnor
+	and		dh, 11111110b
+	or		dh, al ; wpisanie ostatniego bitu z kolejnego bajtu
+
+	xor		dl, dh
+	not		dl ; główna funkcja dekodująca
+
+	ror		dx, 1 ; pierwszy bit dh = xnor 7 i 8 bitu danych
+	and		dl, 01111111b ; wyzerowanie 1 bitu wyniku
+	or		dl, ah ; wpisanie pierwszego bitu
+
+	mov		[edi+ebx], BYTE PTR dl
+
+	inc		ebx
+debug_dalej:
+	loop main_loop
+
 
 
 	popa
+	ret
 _main ENDP
 END
